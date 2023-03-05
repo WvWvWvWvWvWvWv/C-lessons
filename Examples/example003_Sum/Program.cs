0f9d15da45b1792e031547311d667aa7f6431545
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-05 14:12:35 +0300

    Random.next_Sum

--- Examples/example003_Sum/Program.cs
+++ Examples/example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 31;
-int nubmerB = 5;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int nubmerB = new Random().Next(1,10);
+Console.WriteLine(nubmerB);
 int result = numberA + nubmerB;
 Console.WriteLine(result);
 
