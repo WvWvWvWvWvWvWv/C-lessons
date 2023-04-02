d6e75f09f4effbbb53875b1c8b8a917b432801d7
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-04-02 15:04:33 +0300

    '#'

---
+++ Examples/exother/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+Console.Clear();
+string text = "(1,2) (2,3) (4,5) (9,7)"
+            .Replace("(", "")
+            .Replace(")", "");
+System.Console.WriteLine(text);
+var data = text.Split(" ")
+                    .Select(item => item.Split(','))
+                    .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                    .Where(e => e.x % 2 == 0)
+                    .Select(point => (point.x * 10, point.y))
+                    .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    System.Console.WriteLine(data[i]);
+}
